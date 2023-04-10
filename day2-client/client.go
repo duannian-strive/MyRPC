677363d371a04b5c3009ecb5512e5f098a9eddd2
duanniannian <duanniannian1232@gmail.com>
2023-04-10 17:08:12 +0800

    day02 start

---
+++ day2-client/client.go
@@ -0,0 +1,71 @@
+package day2_client
+
+import (
+	myrpc "MyRPC/day1-codec"
+	"MyRPC/day2-client/codec"
+	"errors"
+	"io"
+	"sync"
+)
+
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
+	Done          chan *Call
+}
+
+func (call *Call) done() {
+	call.Done <- call
+}
+
+//Client represent an RPC Client
+// There may be multiple outstanding Calls associated
+// with a single Client,and a Client may be used by
+// multiple goroutines simultaneously
+type Client struct {
+	cc       codec.Codec
+	opt      *myrpc.Option
+	sending  sync.Mutex
+	header   codec.Header
+	mu       sync.Mutex
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  bool
+	shutdown bool
+}
+
+var _ io.Closer = (*Client)(nil)
+var ErrShutdown = errors.New("connection is shut down")
+
+// Close the connection
+func (client *Client) Close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing {
+		return ErrShutdown
+	}
+	client.closing = true
+	return client.cc.Close()
+}
+
+// IsAvailable return true if the client does work
+func (client *Client) IsAvailable() bool {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	return !client.shutdown && !client.closing
+}
+
+func (client *Client) registerCall(call *Call) (uint64, error) {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing || client.shutdown {
+		return 0, ErrShutdown
+	}
+	call.Seq = client.seq
+	client.pending[call.Seq] = call
+	client.seq++
+	return call.Seq, nil
+}
