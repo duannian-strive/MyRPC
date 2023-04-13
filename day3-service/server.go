171aefe7a9de57e74c0ec6ae11fa01347301613c
duanniannian <duanniannian1232@gmail.com>
2023-04-13 21:46:22 +0800

    完成server端的服务注册功能

--- day3-service/server.go
+++ day3-service/server.go
@@ -3,11 +3,12 @@ package day3_service
 import (
 	"MyRPC/day2-client/codec"
 	"encoding/json"
-	"fmt"
+	"errors"
 	"io"
 	"log"
 	"net"
 	"reflect"
+	"strings"
 	"sync"
 )
 
@@ -24,7 +25,47 @@ var DefaultOption = &Option{
 }
 
 // Server represents an RPC Server.
-type Server struct{}
+type Server struct {
+	serviceMap sync.Map
+}
+
+// day3 add start
+
+//Register publishes in the server the set of methods of the
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc: service already defined:" + s.name)
+	}
+	return nil
+}
+
+//Register publishes the receiver's methods in the DefaultServer.
+func Register(rcvr interface{}) error {
+	return DefaultServer.Register(rcvr)
+}
+
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		err = errors.New("RPC service:service/method requestt ill-formed:" + serviceMethod)
+		return
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server:can't find service" + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype = svc.method[methodName]
+	if mtype == nil {
+		err = errors.New("rpc server:can't find method" + methodName)
+	}
+	return
+}
+
+// day03 add end
 
 //NewServer returns a new Server
 func NewServer() *Server {
@@ -101,6 +142,9 @@ func (server *Server) serveCodec(cc codec.Codec) {
 type request struct {
 	h            *codec.Header // header of request
 	argv, replyv reflect.Value // argv and replyv of request
+	// day3 add
+	mtype *methodType
+	svc   *service
 }
 
 func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
@@ -121,11 +165,23 @@ func (server *Server) readRequest(cc codec.Codec) (*request, error) {
 	}
 	req := &request{h: h}
 	// todo:now we dont't know the type of request argv
-	// day 1,just suppose it's string
-	req.argv = reflect.New(reflect.TypeOf(""))
-	if err = cc.ReadBody(req.argv.Interface()); err != nil {
-		log.Println("rpc server: read argv err:", err)
+	//day3 add
+	req.argv = req.mtype.newArgv()
+	req.replyv = req.mtype.newReply()
+	// make sure that argvi is a pointer,ReadBody need a pointer as parameter
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface()
 	}
+	if err = cc.ReadBody(argvi); err != nil {
+		log.Println("rpc server:read body err:", err)
+		return req, err
+	}
+	// day 1,just suppose it's string
+	//req.argv = reflect.New(reflect.TypeOf(""))
+	//if err = cc.ReadBody(req.argv.Interface()); err != nil {
+	//	log.Println("rpc server: read argv err:", err)
+	//}
 	return req, nil
 }
 func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
@@ -139,8 +195,17 @@ func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interfa
 func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
 	//todo,should call registered rpc methods to get the right replyv
 	//day1 1,just print argv and send a hello message
+	//defer wg.Done()
+	//log.Println(req.h, req.argv.Elem())
+	//req.replyv = reflect.ValueOf(fmt.Sprintf("myrpc resp %d", req.h.Seq))
+	//server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+	// day3
 	defer wg.Done()
-	log.Println(req.h, req.argv.Elem())
-	req.replyv = reflect.ValueOf(fmt.Sprintf("myrpc resp %d", req.h.Seq))
+	err := req.svc.call(req.mtype, req.argv, req.replyv)
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(cc, req.h, invalidRequest, sending)
+		return
+	}
 	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
 }
