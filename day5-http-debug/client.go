b7e0b7b5ec7624bf2f0830da12abd864b8869bdc
duanniannian <duanniannian1232@gmail.com>
2023-04-15 10:47:40 +0800

    day5 client支持http协议

--- day5-http-debug/client.go
+++ day5-http-debug/client.go
@@ -2,6 +2,7 @@ package day5_http_debug
 
 import (
 	"MyRPC/day2-client/codec"
+	"bufio"
 	"context"
 	"encoding/json"
 	"errors"
@@ -9,6 +10,8 @@ import (
 	"io"
 	"log"
 	"net"
+	"net/http"
+	"strings"
 	"sync"
 	"time"
 )
@@ -284,5 +287,51 @@ func dialTimeout(f newClientFunc, network, address string, opts ...*Option) (cli
 		return nil, fmt.Errorf("rpc client: connect timeout: expect within %s", opt.ConnectTimeout)
 	case result := <-ch:
 		return result.client, result.err
+	}
+}
+
+//day5
+//NewHTTPClient new a Client instance via HTTP as transport protocol
+
+func NewHTTPClient(conn net.Conn, opt *Option) (*Client, error) {
+	_, _ = io.WriteString(conn, fmt.Sprintf("CONNECT %s HTTP/1.0\n\n", defaultRPCPath))
+
+	// Require successful HTTP response
+	// before switching to RPC protocol
+	resp, err := http.ReadResponse(bufio.NewReader(conn), &http.Request{Method: "CONNECT"})
+	if err == nil && resp.Status == connected {
+		return NewClient(conn, opt)
+	}
+	if err == nil {
+		err = errors.New("unexpected HTTP response:" + resp.Status)
+	}
+	return nil, err
+}
+
+// DailHTTP connects to an HTTP RPC server at the specified network address
+// listening on the default HTTP RPC path
+
+func DialHTTP(network, address string, opts ...*Option) (*Client, error) {
+	return dialTimeout(NewHTTPClient, network, address, opts...)
+}
+
+// xDail call different functions to connect to a RPC server
+// according the first parameter rpcAddr.
+// rpcAddr is a general format (protocol@Addr) to represent a rpc server
+// eg,http@10.0.0.1:7001,tcp@10.0.0.1:9999 unix@/tmp/myRpc.sock
+
+func XDial(rpcAddr string, opts ...*Option) (*Client, error) {
+	parts := strings.Split(rpcAddr, "@")
+	if len(parts) != 2 {
+		return nil, fmt.Errorf("rpc client err:wrong format '%s',expect protocol@addr", rpcAddr)
+	}
+	protocol, addr := parts[0], parts[1]
+	switch protocol {
+	case "http":
+		return DialHTTP("tcp", addr, opts...)
+	default:
+		// tcp ,unix or other transport protocol
+		return Dial(protocol, addr, opts...)
+
 	}
 }
